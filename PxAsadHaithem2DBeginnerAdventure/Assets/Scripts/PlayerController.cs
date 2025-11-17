8a227c2e8d54248a8d4df216efe942264472b8c8
oghaithem <767557@student.dusd.net>
2025-11-17 13:56:19 -0800

    hhygnbrt
    Redid Player controller

--- PxAsadHaithem2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ PxAsadHaithem2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+
+        Vector2 position = ()
+        
         transform.position = position;
    
     }
